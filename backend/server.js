48b29d0d653fe51e36021999405fc7064493ef70
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-04-08 19:41:47 +0100

    backend - register endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 
+import authRoutes from './routes/auth.routes.js'
+
 import connectDB from './config/database.config.js'
 
 dotenv.config()
@@ -8,6 +10,11 @@ dotenv.config()
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api/auth', authRoutes)
 
 app.get('/', () => console.log('Server is ready!'))
 
